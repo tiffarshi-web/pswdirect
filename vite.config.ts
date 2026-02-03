93d7991a52372dae613ff28e1ccb555e7660e540
tiffarshi-web <tiffarshi@gmail.com>
2026-02-03 08:26:02 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,16 +3,12 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
+export default defineConfig({
   server: {
     host: "::",
     port: 8080,
   },
-  plugins: [
-    react(),
-    mode === "development" && componentTagger(),
-  ].filter(Boolean),
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
@@ -21,10 +17,5 @@ export default defineConfig(({ mode }) => ({
   base: "/",
   build: {
     outDir: "dist",
-    rollupOptions: {
-      input: {
-        main: path.resolve(__dirname, "index.html"),
-      },
-    },
   },
-}));
+});
